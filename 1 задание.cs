0f3ee8465f2a0df32a031f23c10eb0fffaf7772b
Ilua <havelailya.com01@gmail.com>
2023-01-12 00:40:21 +0300

    1

---
+++ 1 задание.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace progect
+{
+    class Ilya
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Вы внесли = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Под процентную ставку = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = a + (a * (b / 100));
+            Console.WriteLine("Через год вы получите =", c);
+            
+
+        }
+    }
+}
